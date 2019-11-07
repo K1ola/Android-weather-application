51438685f36b7d6d0ee55396252ef50ac23ef94f
kate <kirilek@bk.ru>
2019-11-08 01:21:32 +0300

    added empty main fragment

---
+++ app/src/main/java/com/example/myapplication/presentation/main/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.myapplication.presentation.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class MainFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/myapplication/presentation/main/main.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication.presentation.main;
-
-public class main {
-    // xml + listeners
-}
