119ae0bc9d3437fd325d716326d33b78fa2535a0
kate <kirilek@bk.ru>
2019-11-12 16:35:51 +0300

    add screaming snake case to consts

--- app/src/main/java/com/example/myapplication/presentation/main/MainFragment.java
+++ app/src/main/java/com/example/myapplication/presentation/main/MainFragment.java
@@ -14,11 +14,11 @@ import androidx.fragment.app.Fragment;
 import com.example.myapplication.R;
 
 public class MainFragment extends Fragment {
-    private static final String temperature = "+29°C";
-    private static final String weatherStatus = "Солнечно";
-    private static final String wet = "Влажность: 29%";
-    private static final String pressure = "Давление: 27 мм. рт. ст.";
-    private static final String wind = "Сила ветра: 10 м/с";
+    private static final String TEMPERATURE = "+29°C";
+    private static final String WEATHER_STATUS = "Солнечно";
+    private static final String WET = "Влажность: 29%";
+    private static final String PRESSURE = "Давление: 27 мм. рт. ст.";
+    private static final String WIND = "Сила ветра: 10 м/с";
 
     @NonNull
     @Override
@@ -31,11 +31,11 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        setViewText(view, R.id.temperature, temperature);
-        setViewText(view, R.id.status, weatherStatus);
-        setViewText(view, R.id.wet, wet);
-        setViewText(view, R.id.pressure, pressure);
-        setViewText(view, R.id.wind, wind);
+        setViewText(view, R.id.temperature, TEMPERATURE);
+        setViewText(view, R.id.status, WEATHER_STATUS);
+        setViewText(view, R.id.wet, WET);
+        setViewText(view, R.id.pressure, PRESSURE);
+        setViewText(view, R.id.wind, WIND);
 
         final ImageView imageView = view.findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.cold_snow_snowflake);
