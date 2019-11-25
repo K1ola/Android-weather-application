896d10d00e43a450112dc8580a9cfac757c86f30
Georgik0 <55991511+Georgik0@users.noreply.github.com>
2019-11-25 15:44:52 +0300

    add toolbar to SettingsFragment

--- app/src/main/java/com/example/myapplication/presentation/settings/SettingsFragment.java
+++ app/src/main/java/com/example/myapplication/presentation/settings/SettingsFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 
 import androidx.annotation.NonNull;
@@ -15,6 +17,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
 import com.example.myapplication.interactor.SettingsViewModel;
+import com.example.myapplication.presentation.main.MainFragment;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -51,7 +54,24 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
         setSwitchHandler(view, R.id.switch_pressure, pressureState);
         setSwitchHandler(view, R.id.switch_wind, windState);
 
+        ImageButton arrow_back = view.findViewById(R.id.arrow_back);
+        arrow_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenMainFragment();
+            }
+        });
+
         return view;
+    }
+
+    private void OpenMainFragment(){
+        MainFragment mainFragment = new MainFragment();
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, mainFragment)
+                .addToBackStack(null)
+                .commit();
     }
 
     private void setSwitchHandler(View view, int idSwitch, boolean state) {
